06d1b4d9038a461177d2c78beba85babbc089cdd
Iván Gómez Pinta <44321109+GomezIvann@users.noreply.github.com>
2024-04-02 16:33:54 +0200

    Nuevo footer para la app

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { CabeceraComponent } from './components/cabecera/cabecera.component';
+import { PiePaginaComponent } from './components/pie-pagina/pie-pagina.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CabeceraComponent, RouterLink, RouterOutlet],
+  imports: [CabeceraComponent, PiePaginaComponent, RouterLink, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
