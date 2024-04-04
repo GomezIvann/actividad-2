57099246582c66f8f62dc53635cdc55e08e82aa5
Iván Gómez Pinta <44321109+GomezIvann@users.noreply.github.com>
2024-04-04 19:57:45 +0200

    Página de inicio añadida

--- frontend/src/app/components/inicio/inicio.component.ts
+++ frontend/src/app/components/inicio/inicio.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { InicioTarjetaComponent } from '../inicio-tarjeta/inicio-tarjeta.component';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, InicioTarjetaComponent],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
-export class InicioComponent {
-
-}
+export class InicioComponent {}
