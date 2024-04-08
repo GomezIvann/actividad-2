e6bfca38de203ce02356204b273158d8b8063465
Iván Gómez Pinta <sondela98@gmail.com>
2024-04-08 23:23:28 +0200

    Reservar cita con wizard de material

---
+++ frontend/src/app/components/reserva-cita/reserva-cita.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reserva-cita',
+  standalone: true,
+  imports: [],
+  templateUrl: './reserva-cita.component.html',
+  styleUrl: './reserva-cita.component.css'
+})
+export class ReservaCitaComponent {
+
+}
--- frontend/src/app/components/reservar-cita/reservar-cita.component.ts
+++ frontend/src/app/components/reservar-cita/reservar-cita.component.ts
@@ -1,22 +1,56 @@
 import { Component } from '@angular/core';
 import { ServicioService } from '../../services/servicio.service';
 import { Servicio } from '../../interfaces/servicio';
-import { CommonModule } from '@angular/common';
 import { Tienda } from '../../interfaces/tienda';
 import { TiendaService } from '../../services/tienda.service';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatIconModule } from '@angular/material/icon';
+import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
 
 @Component({
   selector: 'app-reservar-cita',
   standalone: true,
-  imports: [CommonModule],
+  providers: [
+    {
+      provide: STEPPER_GLOBAL_OPTIONS,
+      useValue: { displayDefaultIndicatorType: false },
+    },
+  ],
+  imports: [CommonModule, ReactiveFormsModule, MatStepperModule, MatIconModule],
   templateUrl: './reservar-cita.component.html',
   styleUrl: './reservar-cita.component.css',
 })
 export class ReservarCitaComponent {
   servicios: Servicio[] = [];
   tiendas: Tienda[] = [];
-  
-  constructor(private _servicioServicio: ServicioService, private _tiendaServicio: TiendaService) {}
+
+  reservarCitaForm = this._formBuilder.group({
+    formArray: this._formBuilder.array([
+      this._formBuilder.group({
+        nombre: ['', Validators.required],
+        apellidos: ['', Validators.required],
+        email: ['', Validators.email],
+        telefono: [''],
+      }),
+      this._formBuilder.group({
+        servicio: ['', Validators.required],
+        empleado: [{ value: '', disabled: true }, Validators.required],
+      }),
+      this._formBuilder.group({
+        fecha: ['', Validators.required],
+        tienda: ['', Validators.required],
+        horario: [{ value: '', disabled: true }, Validators.required],
+      }),
+    ]),
+  });
+
+  constructor(
+    private _servicioServicio: ServicioService,
+    private _tiendaServicio: TiendaService,
+    private _formBuilder: FormBuilder
+  ) {}
 
   ngOnInit(): void {
     this._servicioServicio.obtenerServicios().subscribe((response) => {
@@ -25,5 +59,21 @@ export class ReservarCitaComponent {
     this._tiendaServicio.obtenerTiendas().subscribe((response) => {
       this.tiendas = response.data;
     });
+
+    this.formArray
+      .at(1)
+      .get('servicio')
+      .valueChanges.subscribe(() => {
+        if (this.formArray.at(1).get('servicio').value)
+          this.formArray.at(1).get('empleado').enable();
+      });
+  }
+
+  get formArray() {
+    return this.reservarCitaForm.get('formArray') as any;
+  }
+
+  reservarCita(): void {
+    console.warn(this.reservarCitaForm.value);
   }
 }
