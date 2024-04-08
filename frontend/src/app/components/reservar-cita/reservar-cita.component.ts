60e47bbd2a0ce1d1475a4c1f432deb0c88dc9bf9
Iván Gómez Pinta <44321109+GomezIvann@users.noreply.github.com>
2024-04-08 11:39:05 +0200

    Cambios en el formulario de reserva de citas

--- frontend/src/app/components/reservar-cita/reservar-cita.component.ts
+++ frontend/src/app/components/reservar-cita/reservar-cita.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { ServicioService } from '../../services/servicio.service';
+import { Servicio } from '../../interfaces/servicio';
+import { CommonModule } from '@angular/common';
+import { Tienda } from '../../interfaces/tienda';
+import { TiendaService } from '../../services/tienda.service';
 
 @Component({
   selector: 'app-reservar-cita',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './reservar-cita.component.html',
   styleUrl: './reservar-cita.component.css',
 })
-export class ReservarCitaComponent {}
+export class ReservarCitaComponent {
+  servicios: Servicio[] = [];
+  tiendas: Tienda[] = [];
+  
+  constructor(private _servicioServicio: ServicioService, private _tiendaServicio: TiendaService) {}
+
+  ngOnInit(): void {
+    this._servicioServicio.obtenerServicios().subscribe((response) => {
+      this.servicios = response.data;
+    });
+    this._tiendaServicio.obtenerTiendas().subscribe((response) => {
+      this.tiendas = response.data;
+    });
+  }
+}
